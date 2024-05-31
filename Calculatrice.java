c5367c8b33cc2ad8403d2c44cbcf870313145763
safiya-ng <nguyen.safiya@gmail.com>
2024-05-31 14:08:12 +0000

    Gestion des erreurs: /0

--- Calculatrice.java
+++ Calculatrice.java
@@ -8,11 +8,22 @@ public class Calculatrice {
         Expression dixSept = new Nombre(17D) ;
         Expression s = new Soustraction(dixSept, deux) ;
         Expression a = new Addition(deux, trois) ;
-        Expression d = new Division(s, a) ;
 
         System.out.println(s + " = " + s.valeur()) ; // affiche ((17 - 2) / (2 + 3)) = 3
+        try {
+            Expression d = new Division(s, a) ;
+            System.out.println(d + " = " + d.valeur()) ; // affiche ((17 - 2) / (2 + 3)) = 3
+        
+        }
 
-        System.out.println(d + " = " + d.valeur()) ; // affiche ((17 - 2) / (2 + 3)) = 3
+        catch (DivisionZeroExc e){
+            System.out.println("Expression" + e);
+        }
+        
+
+        
+
+        
 
         
     }
